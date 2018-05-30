37bd97b5e488e502e9cb7f1f553da3cfbfd17c4c
Amabel <luoweibinb@gmail.com>
2018-05-30 22:04:41 +0900

    :sparkles: add message-area.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FriendListComponent } from './friend-list/friend-list.component';
+import { MessageAreaComponent } from './message-area/message-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FriendListComponent
+    FriendListComponent,
+    MessageAreaComponent
   ],
   imports: [
     BrowserModule
