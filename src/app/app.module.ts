8475f6a1c2f2eaafa54fafa4021cdfdf47871cd7
Amabel <luoweibinb@gmail.com>
2018-05-30 22:39:31 +0900

    :sparkles: feat(app-root): use router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { FriendListComponent } from './friend-list/friend-list.component';
 import { MessageAreaComponent } from './message-area/message-area.component';
 import { LoginComponent } from './login/login.component';
@@ -14,9 +15,9 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
