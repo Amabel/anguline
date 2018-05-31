8ad13d63a8fedaae45a4ee43b0796cdadce3d1c7
Amabel <luoweibinb@gmail.com>
2018-05-31 23:12:19 +0900

    :heavy_plus_sign: add dependencies for PrimeNG

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
+  username: string;
 
   constructor() { }
 
