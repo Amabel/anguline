4a63ee584f9c3d030fa39a6a4d03bcb87207c2db
Amabel <luoweibinb@gmail.com>
2018-06-01 23:39:53 +0900

    :truck: feat(home): move friend-list and message-area into home/

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes }  from '@angular/router';
 
-import { FriendListComponent } from './friend-list/friend-list.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
